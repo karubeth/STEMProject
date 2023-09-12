e66c100c20b472623728ed39a64f675f0544ceeb
karungieliza@gmail.com <Karungi Elizabeth>
2023-09-12 22:06:19 +0300

    I really didnt add much

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='blog/images/')
 
     def publish(self):
